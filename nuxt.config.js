6e9ea336b16b889d4e8e3e89de96275399af5ca6
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2021-08-17 19:20:45 +0800

    add line highlights and remove unused langs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,33 +1,33 @@
-import highlightjs from 'highlight.js/lib/common'
+import hljs from 'highlight.js/lib/common'
 import hljsDefineGraphQL from 'highlightjs-graphql'
 
 // register the required languages or add  plugins.
 // Refer to https://github.com/highlightjs/highlight.js/blob/main/SUPPORTED_LANGUAGES.md for the full list
-hljsDefineGraphQL(highlightjs)
+hljsDefineGraphQL(hljs)
 
 // unregister unnecessary languages
-highlightjs.unregisterLanguage('bash')
-highlightjs.unregisterLanguage('cpp')
-highlightjs.unregisterLanguage('php-template')
-highlightjs.unregisterLanguage('yaml')
-highlightjs.unregisterLanguage('python-repl')
-highlightjs.unregisterLanguage('csharp')
-highlightjs.unregisterLanguage('diff')
-highlightjs.unregisterLanguage('ruby')
-highlightjs.unregisterLanguage('go')
-highlightjs.unregisterLanguage('ini')
-highlightjs.unregisterLanguage('java')
-highlightjs.unregisterLanguage('kotlin')
-highlightjs.unregisterLanguage('less')
-highlightjs.unregisterLanguage('lua')
-highlightjs.unregisterLanguage('makefile')
-highlightjs.unregisterLanguage('perl')
-highlightjs.unregisterLanguage('objectivec')
-highlightjs.unregisterLanguage('r')
-highlightjs.unregisterLanguage('rust')
-highlightjs.unregisterLanguage('scss')
-highlightjs.unregisterLanguage('swift')
-highlightjs.unregisterLanguage('vbnet')
+hljs.unregisterLanguage('bash')
+hljs.unregisterLanguage('cpp')
+hljs.unregisterLanguage('php-template')
+hljs.unregisterLanguage('yaml')
+hljs.unregisterLanguage('python-repl')
+hljs.unregisterLanguage('csharp')
+hljs.unregisterLanguage('diff')
+hljs.unregisterLanguage('ruby')
+hljs.unregisterLanguage('go')
+hljs.unregisterLanguage('ini')
+hljs.unregisterLanguage('java')
+hljs.unregisterLanguage('kotlin')
+hljs.unregisterLanguage('less')
+hljs.unregisterLanguage('lua')
+hljs.unregisterLanguage('makefile')
+hljs.unregisterLanguage('perl')
+hljs.unregisterLanguage('objectivec')
+hljs.unregisterLanguage('r')
+hljs.unregisterLanguage('rust')
+hljs.unregisterLanguage('scss')
+hljs.unregisterLanguage('swift')
+hljs.unregisterLanguage('vbnet')
 
 export default {
   target: 'static',
@@ -66,6 +66,7 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
+  loading: false,
   css: ['@assets/css/code.css'],
   plugins: [],
   components: true,
@@ -82,15 +83,50 @@ export default {
     markdown: {
       remarkPlugins: [['remark-emoji', { emoticon: true }]],
       highlighter(rawCode, lang, { fileName, lineHighlights }) {
+        let lines = ''
+        let div = ''
+
+        // custom line highlighter
+        if (lineHighlights) {
+          // split individual and ranged lines
+          const needHighlight = lineHighlights.split(',')
+
+          const lineNumbers = []
+          needHighlight.forEach((line) => {
+            if (line.includes('-')) {
+              // convert strings from range to numbers
+              // eg: 1-4 becomes 1,2,3,4
+              const [num1, num2] = line.split('-').map((i) => Number(i))
+              const diff = num2 - num1 + 1
+
+              lineNumbers.push(
+                ...Array(diff)
+                  .fill()
+                  .map((_, i) => num1 + i)
+              )
+            } else {
+              lineNumbers.push(Number(line))
+            }
+          })
+
+          const emptyLines = Array(rawCode.match(/\n/g).length).fill('<br>')
+          emptyLines.forEach((l, i) => {
+            lines += lineNumbers.includes(i + 1)
+              ? '<div class="highlight-line"> </div>'
+              : l
+          })
+          div = emptyLines ? `<div class="highlight">${lines}</div>` : ''
+        }
+
         const highlightedCode = (
           lang
-            ? highlightjs.highlight(rawCode, { language: lang })
-            : highlightjs.highlightAuto(rawCode)
+            ? hljs.highlight(rawCode, { language: lang })
+            : hljs.highlightAuto(rawCode)
         ).value
 
-        return `<pre class="nuxt-content-highlight">${
-          fileName ? `<span class="filename">${fileName}</span>` : ''
-        }<code class="hljs ${lang}">${highlightedCode}</code></pre>`
+        const filename = `<span class="filename">${fileName || lang}</span>`
+
+        return `<pre>${div}${filename}<code class="hljs ${lang}">${highlightedCode}</code></pre>`
       }
     },
     nestedProperties: ['author.name']
